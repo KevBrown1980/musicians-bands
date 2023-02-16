26b09b5a1b5a6e8a04346b7d0c07464fe11bfd00
KevBrown1980 <kevinpatonbrown@gmail.com>
2023-02-16 16:16:23 +0000

    quick update

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const {Band} = require('./Band')
 const {Musician} = require('./Musician')
 
+
+// one-to-one relationship
+Musician.belongsTo(Band);
+// one-to-many relationship
+Band.hasMany(Musician);
+
+
 module.exports = {
     Band,
     Musician
--- index.test.js
+++ index.test.js
@@ -20,8 +20,8 @@ describe('Band and Musician Models', () => {
         })
     
         test('can create a Band', async () => {
-            const testUser3 = await Band.create({ name: 'Oasis', genre: 'Rock and Pop' });
-            expect(testUser3.genre).toBe('Rock and Pop');
+            const testUser3 = await Band.create({ name: 'Blur', genre: 'BritPop' });
+            expect(testUser3.genre).toBe('BritPop');
         })
 
 
@@ -32,8 +32,26 @@ describe('Band and Musician Models', () => {
         })
 
         test('can create a Musician', async () => {
-            const testUser4 = await Musician.create({ name: 'Noel', instrument: 'Guitar' });
-            expect(testUser4.instrument).toBe('Guitar');
+            const testUser4 = await Musician.create({ name: 'Damian', instrument: 'Singer' });
+            expect(testUser4.instrument).toBe('Singer');
         })
+
+        test('test if musicians are in band', async () => {
+            
+            
+// User to add post to
+const someMusician = await Musician.findByPk(1)
+// Adding the post with an id of 4 to someUser
+await someMusician.addBand(1)
+// Verifying it's been added
+const result = await Band.findByPk(1);
+//console.log(posts)
+
+           
+
+            expect(result).toBe('xyz');
+        })
+
+
     
 })
